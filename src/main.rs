a120e24bd61729864fdd84b0eecac3a7533aa8aa
Vineel Kovvuri <vineel.kovvuri@gmail.com>
2024-03-17 00:39:09 -0700

    Hook up Matcher trait object
    Signed-off-by: Vineel Kovvuri <vineel.kovvuri@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -18,13 +18,16 @@ impl<'a> Matcher for ExactFileNameMatcher<'a> {
 fn main() {
     let mut results: Vec<String> = Vec::new();
     let path = Path::new("C:\\");
-    search(&path, "kernel32.dll", &mut results);
+    let exact_matcher = ExactFileNameMatcher {
+        file_name: "kernel32.dll",
+    };
+    search(&path, &exact_matcher, &mut results);
     for result in results {
         println!("{:?}", result);
     }
 }
 
-fn search(path: &Path, file_name: &str, results: &mut Vec<String>) {
+fn search(path: &Path, matcher: &dyn Matcher, results: &mut Vec<String>) {
     let result = fs::read_dir(&path);
     if let Ok(entries) = result {
         for entry in entries {
@@ -32,8 +35,8 @@ fn search(path: &Path, file_name: &str, results: &mut Vec<String>) {
                 let sub_path = entry.path();
                 if sub_path.is_dir() {
                     let sub_path = Path::new(&sub_path);
-                    search(sub_path, file_name, results);
-                } else if sub_path.ends_with(file_name) {
+                    search(sub_path, matcher, results);
+                } else if matcher.match_path(sub_path.to_str().unwrap()) {
                     results.push(String::from(sub_path.as_os_str().to_str().unwrap()));
                 }
             }
